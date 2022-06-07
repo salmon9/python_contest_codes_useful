d09ae2d26ba959591e1a769200e190b28f7ad7dc
salmon9 <49511344+salmon9@users.noreply.github.com>
2022-06-07 15:46:21 +0900

    Create sample_1.py

---
+++ sample_1.py
@@ -0,0 +1,22 @@
+import sys
+2
+3def main(n ,m ,k, c ,u):
+4    flag = 0
+5    for i in range(n):
+6        for j in range(m):
+7            c[i]
+8
+9if __name__ == '__main__':
+10
+11    c = u = []
+12
+13    n, m, k = map(int, input().split())  #n:地点のかず m:道の数 k:時刻
+14    c = list(map(int, input().split()))
+15    for i in range(m):
+16        u.append(list(map(int, input().split())))
+17    print(n ,m ,k)
+18    print(c)
+19    print(u)
+20
+21
+22    main(n ,m ,k, c ,u)
